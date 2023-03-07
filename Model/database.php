b8832a84470fd30668bbf1533d4d7ec1f832c2af
aebish01 <69996497+aebish01@users.noreply.github.com>
2023-03-06 23:33:24 -0800

    Update database.php

--- Model/database.php
+++ Model/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = "mysql://wsizpmpv1b4yo1k1:gdvkq1ywvxzeo89j@j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/savummwta2kz104x";
+$dsn = "mysql:host=j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=savummwta2kz104x";
 $username = 'wsizpmpv1b4yo1k1';
 $password = 'gdvkq1ywvxzeo89j';
 
