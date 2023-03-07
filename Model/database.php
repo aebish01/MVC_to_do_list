ec85409388a7a10b65da25a7be215fef09d89866
aebish01 <69996497+aebish01@users.noreply.github.com>
2023-03-06 23:26:29 -0800

    Update database.php

--- Model/database.php
+++ Model/database.php
@@ -1,10 +1,11 @@
 <?php
 
-$dsn = "mysql:host=localhost; dbname=todolist";
-$username = 'root';
+$dsn = "mysql://wsizpmpv1b4yo1k1:gdvkq1ywvxzeo89j@j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/savummwta2kz104x";
+$username = 'wsizpmpv1b4yo1k1';
+$password = 'gdvkq1ywvxzeo89j';
 
 try {
-    $db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $password);
 } catch (PDOException $e) {
     $error_message = "Database Error: ";
     $error_message .= $e->getMessage();
